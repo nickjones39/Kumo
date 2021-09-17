5dda4e82d62a1bbc7dcbc439a171b0c51a20e11f
Nick <nj78@students.waikato.ac.nz>
2021-09-17 23:51:06 +1200

    removed webhook

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ var Slack = require('slack-node');
 const fetch = require('node-fetch');
 
 
-webhookUri = "https://hooks.slack.com/services/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";
+
 const api = "https://u50g7n0cbj.execute-api.us-east-1.amazonaws.com/v2/locations?limit=1&page=1&offset=0&sort=desc&radius=1000&country_id=NZ&order_by=lastUpdated&dumpRaw=false"
 
 let settings = { method: "Get" };
